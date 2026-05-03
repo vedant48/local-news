abce53ed3059a210851745ec79ad1b7f3f56e49f
vedant48 <vedant@iiitmanipur.ac.in>
2026-05-03 23:40:08 +0530

    chore: update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://local-news-pied.vercel.app',
   vite: {
     plugins: [tailwindcss()]
   },

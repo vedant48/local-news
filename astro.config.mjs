1f89e6c920e0b04b1ef136d466307f3d0d840400
vedant48 <vedant@iiitmanipur.ac.in>
2026-05-03 23:38:29 +0530

    chore: add @astrojs/sitemap and @astrojs/vercel dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
   output: 'server',
-  integrations: [react()]
+  adapter: vercel(),
+  integrations: [react(), sitemap()],
 });
